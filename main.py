19e76dfa0b1b74e89c2867a6e397bc46475dcf68
Melih Ocakcı <melihocakci@gmail.com>
2022-05-08 01:11:33 +0300

    added different methods

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-import pandas as pd
+from ast import Or
+import pandas as pd # for data manipulation
+import numpy as np # for data manipulation
 
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split # for splitting the data into train and test samples
+from sklearn.metrics import classification_report # for model evaluation metrics
+from sklearn.preprocessing import OrdinalEncoder # for encoding categorical features from strings to number arrays
+from sklearn.preprocessing import LabelEncoder
+
+# Differnt types of Naive Bayes Classifiers
 from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import CategoricalNB
+from sklearn.naive_bayes import BernoulliNB
 
 df = pd.read_csv('heart_2020_cleaned.csv')
-data = df[['BMI','PhysicalHealth','MentalHealth','SleepTime']]
-target = df['HeartDisease']
+df = df.to_numpy()
 
-X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.1, random_state=0)
+#x = df[:,[1,5,6,14]] # Gaussian
+#x = df[:,[2,3,4,7,8,12,15,16,17]] # Bernoulli
+x = df[:,[9,10,11,13]] # Categorical
+y = df[:,0]
 
-model = GaussianNB()
-model.fit(X_train, y_train)
+enc = LabelEncoder()
+y = enc.fit_transform(y)
 
-y_pred = model.predict(X_test)
+enc = OrdinalEncoder()
+x = enc.fit_transform(x)
 
-print("Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (y_test != y_pred).sum()))
+a = x[0,0]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+#model = GaussianNB()
+#model = BernoulliNB()
+model = CategoricalNB()
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+
+print("Number of mislabeled points out of a total %d points : %d" % (x_test.shape[0], (y_test != y_pred).sum()))
