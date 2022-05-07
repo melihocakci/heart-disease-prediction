1eeb48f33ccefe1a0dbbad04abca630c1a913a3f
Melih Ocakcı <melihocakci@gmail.com>
2022-05-07 20:59:31 +0300

    gaussian naive bayes

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
 from sklearn import metrics
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
 df = pd.read_csv('heart_2020_cleaned.csv')
 data = df[['BMI','PhysicalHealth','MentalHealth','SleepTime']]
 target = df['HeartDisease']
 
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.1, random_state=0)
+
 model = GaussianNB()
-model.fit(data, target)
+model.fit(X_train, y_train)
 
-print(model)
+y_pred = model.predict(X_test)
 
-expected = target
-predicted = model.predict(data)
-print(metrics.classification_report(expected, predicted))
-print(metrics.confusion_matrix(expected, predicted))
+print("Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (y_test != y_pred).sum()))
