b9854b9be30f6aa6bf02e9e0584ab7b91fc8e50d
Melih Ocakcı <melihocakci@gmail.com>
2022-05-08 01:24:08 +0300

    changed categorical to multinomial

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ast import Or
 import pandas as pd # for data manipulation
-import numpy as np # for data manipulation
+import numpy as np
+from sklearn import multiclass # for data manipulation
 
 from sklearn.model_selection import train_test_split # for splitting the data into train and test samples
 from sklearn.metrics import classification_report # for model evaluation metrics
@@ -9,7 +10,7 @@ from sklearn.preprocessing import LabelEncoder
 
 # Differnt types of Naive Bayes Classifiers
 from sklearn.naive_bayes import GaussianNB
-from sklearn.naive_bayes import CategoricalNB
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
 
 df = pd.read_csv('heart_2020_cleaned.csv')
@@ -17,7 +18,7 @@ df = df.to_numpy()
 
 #x = df[:,[1,5,6,14]] # Gaussian
 #x = df[:,[2,3,4,7,8,12,15,16,17]] # Bernoulli
-x = df[:,[9,10,11,13]] # Categorical
+x = df[:,[9,10,11,13]] # Multinomial
 y = df[:,0]
 
 enc = LabelEncoder()
@@ -32,7 +33,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 
 #model = GaussianNB()
 #model = BernoulliNB()
-model = CategoricalNB()
+model = MultinomialNB()
 model.fit(x_train, y_train)
 
 y_pred = model.predict(x_test)
