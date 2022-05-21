56b37dd4e553cafa1f016f15e5140fa3c2cd78ac
Melih Ocakcı <melihocakci@gmail.com>
2022-05-21 16:17:07 +0300

    about to balance the dataset

--- analyze.py
+++ analyze.py
@@ -1,34 +1,44 @@
+# %%
 import pandas as pd # for data manipulation
 import numpy as np
 
 import matplotlib.pyplot as plt
 from matplotlib import style
 
-################################################################################################################
-
+# %%
 df = pd.read_csv('heart_2020_cleaned.csv')
 
-################################################################################################################
+# %%
+print(df.dtypes)
 
-print(df)
+# %%
+print(df['HeartDisease'].value_counts())
 
-################################################################################################################
+# %%
+df.describe()
 
-plt.style.use('fivethirtyeight')
+# %%
+print(df.info())
+
+# %%
+truedf = df.query("HeartDisease == 'Yes'")
+falsedf = df.query("HeartDisease == 'No'")
+
+# %%
+plt.style.use('ggplot')
 
 num = df['HeartDisease'].value_counts()
 
-plt.bar(['Yes', 'No'], [num.Yes, num.No])
+plt.bar(['Yes', 'No'], [truedf.shape[0], falsedf.shape[0]])
 
 plt.xlabel('Has Heart Disease')
 plt.ylabel('Number of People')
 plt.title('Number of People With Heart Disease')
 
-plt.tight_layout()
-
-plt.savefig('./fig/HeartDisease.png')
+plt.show()
 
 plt.clf()
 
-################################################################################################################
 
+
+# %%
--- predict.py
+++ predict.py
@@ -1,33 +1,26 @@
+# %%
 import pandas as pd # for data manipulation
 import numpy as np
-from sklearn import naive_bayes
+import seaborn as sn
 
-from sklearn.model_selection import train_test_split # for splitting the data into train and test samples
-from sklearn.metrics import classification_report, confusion_matrix # for model evaluation metrics
-from sklearn.preprocessing import OrdinalEncoder # for encoding categorical features from strings to number arrays
-from sklearn.preprocessing import LabelEncoder
-
-import plotly.express as px  # for data visualization
-import plotly.graph_objects as go # for data visualization
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import OrdinalEncoder, LabelEncoder
 
 import matplotlib.pyplot as plt
 
 # Differnt types of Naive Bayes Classifiers
-from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import CategoricalNB
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-################################################################################################################
-
+# %%
 df = pd.read_csv('heart_2020_cleaned.csv')
 
-df['BMI'] = pd.qcut(df['BMI'], 6, duplicates='drop')
-df['PhysicalHealth'] = pd.qcut(df['PhysicalHealth'], 6, duplicates='drop')
-df['MentalHealth'] = pd.qcut(df['MentalHealth'], 6, duplicates='drop')
-df['SleepTime'] = pd.qcut(df['SleepTime'], 6, duplicates='drop')
+df['BMI'] = pd.qcut(df['BMI'], 5, duplicates='drop')
+df['PhysicalHealth'] = pd.qcut(df['PhysicalHealth'], 5, duplicates='drop')
+df['MentalHealth'] = pd.qcut(df['MentalHealth'], 5, duplicates='drop')
+df['SleepTime'] = pd.qcut(df['SleepTime'], 5, duplicates='drop')
 
 y = df['HeartDisease']
 x = df.drop('HeartDisease', axis=1)
@@ -40,21 +33,42 @@ x = enc.fit_transform(x)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.15)
 
-################################################################################################################
-
-model = naive_bayes(max_depth=890, max_features='log2', min_samples_leaf=6)
+# %%
+model = DecisionTreeClassifier()
 
 model.fit(x_train, y_train)
 
 y_pred = model.predict(x_test)
 
-################################################################################################################
+report = classification_report(y_test, y_pred, target_names=['No','Yes'], output_dict=True)
 
 print("Model: ", model)
-print('--------------------------------------------------------')
-report = classification_report(y_test, y_pred, target_names=['No','Yes'])
-print('Classification Report:\n\n', report)
-print('--------------------------------------------------------')
-print('Confusion Matrix:\n\n', confusion_matrix(y_test, y_pred))
+print('Classification Report:\n\n', classification_report(y_test, y_pred, target_names=['No','Yes']))
 
-################################################################################################################
+# %%
+model = CategoricalNB()
+
+model.fit(x_train, y_train)
+
+y_pred = model.predict(x_test)
+
+report = classification_report(y_test, y_pred, target_names=['No','Yes'], output_dict=True)
+
+print("Model: ", model)
+print('Classification Report:\n\n', classification_report(y_test, y_pred, target_names=['No','Yes']))
+
+mat = confusion_matrix(y_test, y_pred, normalize='true')
+
+tmp = mat[0][0]
+mat[0][0] = mat[1][1]
+mat[1][1] = tmp
+
+tmp = mat[0][1]
+mat[0][1] = mat[1][0]
+mat[1][0] = tmp
+
+plt.figure(figsize = (10,7))
+sn.set(font_scale=1.4)
+sn.heatmap(mat, annot=True)
+
+plt.show()
